e0ea2d76ed23c966dd53a8e8e6194c4337ed53ba
Đào Huy <huya2k22@gmail.com>
2023-02-03 01:58:51 +0700

    first commit

---
+++ WebBanHangOnline/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        WebBanHangOnlineEntities db = new WebBanHangOnlineEntities();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var items = db.tb_Category;
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]    
+        
+        public ActionResult Add(tb_Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                
+                db.tb_Category.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.tb_Category.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(tb_Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_Category.Attach(model);
+                
+                model.ModifiedDate = DateTime.Now;
+                db.Entry(model).Property(x => x.Title).IsModified = true;
+                db.Entry(model).Property(x => x.Description).IsModified = true;
+                db.Entry(model).Property(x => x.Alias).IsModified = true;
+                db.Entry(model).Property(x => x.SeoDescription).IsModified = true;
+                db.Entry(model).Property(x => x.SeoKeywords).IsModified = true;
+                db.Entry(model).Property(x => x.SeoTitle).IsModified = true;
+                db.Entry(model).Property(x => x.Position).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+                db.Entry(model).Property(x => x.Modifiedby).IsModified = true;
+
+                
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.tb_Category.Find(id);
+            if(item!= null)
+            {
+                //var DleteItem=db.tb_Category.Attach(item);
+                db.tb_Category.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new {success = false});
+        }
+    }
+}
---
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        WebBanHangOnlineEntities db = new WebBanHangOnlineEntities();
+
+        // GET: Admin/New
+        public ActionResult Index()
+        {
+            var items=db.tb_News.OrderByDescending(x=>x.Id).ToList(); //OrderByDescending: bản ghi mới nhất lên đầu
+            return View(items);
+        }
+        //nhúng các thư viện
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(tb_News model)
+        {
+            if (ModelState.IsValid) 
+            {
+                model.CreatedDate = DateTime.Now;
+                model.CategoryId = 1006;
+                model.ModifiedDate = DateTime.Now;
+                
+                db.tb_News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = db.tb_News.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(tb_News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+               
+                model.ModifiedDate = DateTime.Now;
+
+                db.tb_News.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
